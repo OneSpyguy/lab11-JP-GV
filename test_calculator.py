7da87c090ce2e5a7d47ef9b73aadfbc6aa6a160d
ImJaee <149006049+ImJaee@users.noreply.github.com>
2026-04-13 14:50:39 -0400

    Add unit tests for calculator functions

--- test_calculator.py
+++ test_calculator.py
@@ -3,13 +3,15 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): 
+        self.assertEqual(add(2, 3), 5)      
+        self.assertEqual(add(-1, 1), 0)    
+        self.assertEqual(add(0, 0), 0)  
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
+    def test_subtract(self): 
+        self.assertEqual(subtract(5, 3), 2)    
+        self.assertEqual(subtract(0, 5), -5)   
+        self.assertEqual(subtract(-1, -1), 0)
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
     #     fill in code
@@ -19,19 +21,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):  # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):  # 3 assertions
+        self.assertEqual(logarithm(5, 25), 2)    
+        self.assertEqual(logarithm(10, 100), 2) 
+        self.assertEqual(logarithm(2, 8), 3)    
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):  
+        with self.assertRaises(ValueError):
+            logarithm(1, 5)  
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
